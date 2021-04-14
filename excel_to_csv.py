9f064079723bb896644b1261773372ff68c394a6
Wilson Montenegro <wilmontenegro@me.com>
2021-04-13 23:04:59 -0500

    Ruta y CSV Export Listo

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -4,17 +4,22 @@ import glob
 
 all_files=[]
 
-for f in glob.glob("*.xlsx"):
+#Colectando todos los archivos
+for f in glob.glob('/Users/wilmontenegro/Downloads/*.xlsx'):
     df=pd.read_excel(f)
     all_files.append(pd.read_excel(f))
 
 df = pd.concat(all_files, ignore_index='True')
 print(df.head(10))
 
-save_excel = pd.ExcelWriter('Todo.xlsx')
-df.to_excel(save_excel,'Hoja 1')
-save_excel.save()
+#Para exportar a Excel
+#save_excel = pd.ExcelWriter('/Users/wilmontenegro/Downloads/Todo.xlsx')
+#df.to_excel(save_excel,'Hoja 1')
+#save_excel.save()
 
-#read_file = pd.read_excel (r'Path where the Excel file is stored\File name.xlsx')
-#read_file.to_csv (r'Path to store the CSV file\File name.csv', index = None, header=True)
+#Para exportar a CSV
+save_csv = pd.DataFrame(df)
+save_csv.to_csv('/Users/wilmontenegro/Downloads/Todo.csv', index = None, header=True)
+
+
 
