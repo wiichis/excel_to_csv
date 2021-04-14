aa7e4b465db93e0e1471b923918c5144570cab2b
Wilson Montenegro <wilmontenegro@me.com>
2021-04-14 00:37:25 -0500

    Contandor de filas de cada archivo

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -7,19 +7,22 @@ all_files=[]
 #Colectando todos los archivos
 for f in glob.glob('/Users/wilmontenegro/Downloads/*.xlsx'):
     df=pd.read_excel(f)
+    df.iloc[[0,1]]
+    print(f'El total de filas del archivo {f[31::]} es:', df.shape[0])
     all_files.append(pd.read_excel(f))
 
 df = pd.concat(all_files, ignore_index='True')
 print(df.head(10))
+print(f'El total de filas consolidadas es', df.shape[0])
 
 #Para exportar a Excel
-#save_excel = pd.ExcelWriter('/Users/wilmontenegro/Downloads/Todo.xlsx')
-#df.to_excel(save_excel,'Hoja 1')
-#save_excel.save()
+save_excel = pd.ExcelWriter('/Users/wilmontenegro/Downloads/Todo.xlsx')
+df.to_excel(save_excel,'Hoja 1')
+save_excel.save()
 
 #Para exportar a CSV
-save_csv = pd.DataFrame(df)
-save_csv.to_csv('/Users/wilmontenegro/Downloads/Todo.csv', index = None, header=True)
+#save_csv = pd.DataFrame(df)
+#save_csv.to_csv('/Users/wilmontenegro/Downloads/Todo.csv', index = None, header=True)
 
 
 
