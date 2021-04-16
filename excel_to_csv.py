ac0b6a8cda8501bef5000621627f0d9b1b943555
Wilson Montenegro <wilmontenegro@me.com>
2021-04-15 20:18:44 -0500

    Puede leer xls y xlsx

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -5,7 +5,7 @@ import glob
 all_files=[]
 
 #Colectando todos los archivos
-for f in glob.glob('/Users/wilmontenegro/Downloads/*.xlsx'):
+for f in glob.glob('/Users/wilmontenegro/Downloads/*.xls*'):
     df=pd.read_excel(f)
     df.iloc[[0,1]]
     print(f'El total de filas del archivo {f[31::]} es:', df.shape[0])
@@ -17,7 +17,7 @@ print(f'El total de filas consolidadas es', df.shape[0])
 
 #Para exportar a Excel
 save_excel = pd.ExcelWriter('/Users/wilmontenegro/Downloads/Todo.xlsx')
-df.to_excel(save_excel,'Hoja 1')
+df.to_excel(save_excel,'Data_Total')
 save_excel.save()
 
 #Para exportar a CSV
