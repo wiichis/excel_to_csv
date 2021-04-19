7a2c408dfcd7902021cb76c37afa71ec65f68cd6
Wilson Montenegro <wilmontenegro@me.com>
2021-04-19 13:00:56 -0500

    Modificnado Headers y Campos a usar

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -3,13 +3,14 @@ import numpy as np
 import glob
 
 all_files=[]
-colums = ['CIA','FCH_PROCESO','PEDIDO','ZONA','CODCLIENTE','APELLIDOS','NOMBRE1','NOMBRE2','DIRECCION','TELEFONO','DEPART','PROV','DISTRITO' ]
+columns_preliquidacion = ['CIA','FCH_PROCESO','PEDIDO','ZONA','CODCLIENTE','APELLIDOS','NOMBRE1','NOMBRE2','DIRECCION','TELEFONO','DEPART','PROV','DISTRITO' ]
+columns_prueba_entrega = ['Fecha Facturación', 'Fecha Promesa Entrega', 'Fecha Entrega', 'Compañía', 'Campaña', 'Zona', '# Orden', 'Código Consultora', 'Nombre Consultora', 'Estado Orden', 'Observaciones', 'Imágenes']
 
-#Colectando todos los archivos
+#Colectando todos los archivos, index_Col es para que no ponga un index por defecto, header para seleccionar en que fila estan los titulos
 for f in glob.glob('/Users/wilmontenegro/Downloads/*.xls*'):
-    df=pd.read_excel(f, usecols=colums)
+    df=pd.read_excel(f, header=1, usecols=columns_prueba_entrega)
     print(f'El total de filas del archivo {f[31::]} es:', df.shape[0])
-    all_files.append(pd.read_excel(f, usecols=colums))
+    all_files.append(pd.read_excel(f, header=1, usecols=columns_prueba_entrega))
 
 df = pd.concat(all_files, ignore_index='True')
 print(df.head(10))
